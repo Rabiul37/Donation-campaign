8e1e4ce3c96dadcd3a25de607b0cf854949a953e
Rabiul37 <hmrabiul37@gmail.com>
2023-09-25 20:15:34 +0600

    donation page add data see more and seeless set

--- src/Pages/Donation.jsx
+++ src/Pages/Donation.jsx
@@ -4,30 +4,48 @@ import DonationListCard from "./DonationListCard";
 const Donation = () => {
   const [Donations, setDonations] = useState([]);
   const [noDataFond, setNoDataFond] = useState(false);
-
+  const [isShow, setisShow] = useState(false);
+  console.log(isShow);
   useEffect(() => {
     const DonationsItems = JSON.parse(localStorage.getItem("Donations"));
 
     if (DonationsItems) {
       setDonations(DonationsItems);
     } else {
-      setNoDataFond("no data fond");
+      setNoDataFond("No data fond");
     }
   }, []);
   return (
     <div>
       {noDataFond ? (
-        <p>{noDataFond}</p>
+        <p className=" h-[80vh] flex justify-center items-center text-3xl font-bold text-red-600">
+          {noDataFond}
+        </p>
       ) : (
         <div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-8">
-            {Donations.map((Donation) => (
-              <DonationListCard
-                key={Donation.id}
-                Donation={Donation}
-              ></DonationListCard>
-            ))}
+            {isShow
+              ? Donations.map((Donation) => (
+                  <DonationListCard
+                    key={Donation.id}
+                    Donation={Donation}
+                  ></DonationListCard>
+                ))
+              : Donations.slice(0, 2).map((Donation) => (
+                  <DonationListCard
+                    key={Donation.id}
+                    Donation={Donation}
+                  ></DonationListCard>
+                ))}
           </div>
+          {Donations.length > 2 && (
+            <button
+              className="mt-8 text-xl text-white font-medium bg-orange-500 p-2 rounded block mx-auto"
+              onClick={() => setisShow(!isShow)}
+            >
+              {isShow ? "See Less" : "See More "}
+            </button>
+          )}
         </div>
       )}
     </div>
